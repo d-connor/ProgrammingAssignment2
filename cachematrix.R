40b7ecf9b2559eff0cf7136ee5bfecb4584e01ca
d-connor <derek.connor@gmail.com>
2014-09-20 13:53:58 -0400

    Final Working version of cachematrix.R for Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## I approached this at fist waaaay over thinking it.  The light bulb turned turn on after ## walking through the example step by step and realizing how close it needed to be to the ## actual function.
 ## 
 
-## Write a short comment describing this function
+## Beginning with the first part of the assignment:
 
-makeCacheMatrix <- function(x = matrix()) {  ## The function below assess a matrix
+makeCacheMatrix <- function(x = matrix()) {  ## The function below assess a matrix, needs to be a square matrix (1:4, 2)
 
   
   i <- NULL  ## You want to set i to null, so the parent environment doesn’t overweight this with a value you don’t want
@@ -14,17 +14,17 @@ makeCacheMatrix <- function(x = matrix()) {  ## The function below assess a matr
   get <- function() x  ## Calls upon the value of the matrix
   setinv <- function(solve) i <<- solve  ## Takes value of matrix, inverts it
   getinv <- function() i
-  list(set = set, get = get, setinv = setinv, getinv = getinv)  ## Calls upon the value of the inverses matrix
+  list(set = set, get = get, setinv = setinv, getinv = getinv)  ## Calls upon the value of the inverse matrix
   
 }
 
 
-## This was much easier after completing the first one:
+## This was much easier after completing the first one. This double checks if the environment has cached the matrix/inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   i<- x$getinv()  ##get the inverse previs matrix
-  if(!is.null(i)) {  ## this double checks if the matrix is in the environment 
+  if(!is.null(i)) {  ## this double checks if the matrix is in the environment, specifically the 'opposite' of i being Null (aka present)
     message("Getting Cached Data, Lazy!!")
     return(i)
   }
